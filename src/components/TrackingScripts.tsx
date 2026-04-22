deee42d6191a2b667121c1b25903417525c90dc0
robinshins <robinshins674@gmail.com>
2026-04-23 01:58:09 +0900

    메타 픽셀 변경

--- src/components/TrackingScripts.tsx
+++ src/components/TrackingScripts.tsx
@@ -33,13 +33,13 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1567170677613874');
+fbq('init', '2658141041245734');
 fbq('track', 'PageView');`
       }} />
       <noscript>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img height="1" width="1" style={{display:'none'}} alt=""
-          src="https://www.facebook.com/tr?id=1567170677613874&ev=PageView&noscript=1" />
+          src="https://www.facebook.com/tr?id=2658141041245734&ev=PageView&noscript=1" />
       </noscript>
       {/* TikTok Pixel */}
       <script dangerouslySetInnerHTML={{
